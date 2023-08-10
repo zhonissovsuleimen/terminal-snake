4ff7ea0e6b25db9b3ffc8847a580522e44d1f2ef
Suleimen Zhonissov <lmfao159753@mail.ru>
2023-08-10 22:51:35 +0500

    game loop reworked (still a little buggy)

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,70 @@ use std::time::Duration;
 use crossterm::{
     queue,
     terminal::{Clear, ClearType},
-    cursor::{MoveTo, Hide}
+    cursor::{MoveTo, Hide},
+    event::{poll, read, Event, KeyCode, KeyEvent}
 };
 
+
 mod snake;
-use snake::game::Game;
+use snake::{
+    game::Game,
+    direction::Direction
+};
 
 fn main() {
     let mut stdout = stdout();
     
-    const TICK_DELAY_MS: u64 = 200; 
+    const tick_ms: i32 = 175;
+    let mut timer: i32 = tick_ms;
     let game = &mut Game::new(40, 23);
-
+    let mut new_direction: Direction = Direction::Right;
     hide_cursor(&mut stdout);
     //game loop
     loop{
-        clear_terminal(&mut stdout);
-        game.display(&mut stdout);
-        game.make_move();
-        //executing command queue 
+        match poll(Duration::from_millis(1)){
+            Ok(true) => {
+                let asd_event = read().unwrap();
+
+                new_direction = match asd_event.into() {
+                    Event::Key(KeyEvent{ 
+                        code: KeyCode::Up | KeyCode::Char('w'), .. }) => {
+                            Direction::Up
+                    }
+                    Event::Key(KeyEvent{ 
+                        code: KeyCode::Right | KeyCode::Char('d'), .. }) => {
+                            Direction::Right
+                    }
+                    Event::Key(KeyEvent{ 
+                        code: KeyCode::Down | KeyCode::Char('s'), .. }) => {
+                            Direction::Down
+                        }
+                        Event::Key(KeyEvent{ 
+                            code: KeyCode::Left | KeyCode::Char('a'), .. }) => {
+                            Direction::Left
+                        }
+                    _ => {new_direction}
+                };
+                timer -= 1;
+            },
+            _ =>{
+                timer -= 1;
+            }
+        }
+        if timer < 0 {
+            clear_terminal(&mut stdout);
+            game.change_direction(new_direction); 
+            game.make_move();
+            game.display(&mut stdout);
+            timer = tick_ms;
+        }
+
+
+
+
+        
+        //executing command queue
         stdout.flush().expect("Error while flushing stdout");
-        thread::sleep(Duration::from_millis(TICK_DELAY_MS));
     }
 }
 
